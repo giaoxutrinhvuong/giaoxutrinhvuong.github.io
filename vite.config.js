0bba0505cf284234fa80e872dabd0c1aaf2e41e2
giaoxutrinhvuong <turin@giaoxutrinhvuong.com>
2025-02-02 22:13:26 -0600

    Removing unneeded line

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
-import basicSsl from '@vitejs/plugin-basic-ssl';
 
 // https://vitejs.dev/config/
 export default defineConfig({
