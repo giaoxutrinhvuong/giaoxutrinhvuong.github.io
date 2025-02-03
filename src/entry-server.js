610a235ddd99d64581cbebed47f70cfdb5748098
giaoxutrinhvuong <turin@giaoxutrinhvuong.com>
2025-02-02 23:35:43 -0600

    Trying replacing VN characters with EN characters

--- src/entry-server.js
+++ src/entry-server.js
@@ -2,7 +2,7 @@ import a from './Index.svelte';
 import b from './Đoàn Thể.svelte';
 //import c from './Giáo Xứ.svelte';
 //import d from './Mục Vụ.svelte';
-import e from './phụng vụ.svelte';
+import e from './phung vu.svelte';
 //import f from './Sinh Hoạt.svelte';
 //import g from './Thông Tin.svelte';
 
@@ -15,7 +15,7 @@ export async function renderHTML(file) {
     //  return c.render();
     //} else if (file === 'Mục Vụ.html') {
     //  return d.render();
-  } else if (file === 'phụng vụ.html') {
+  } else if (file === 'phung vu.html') {
     return e.render();
     //} else if (file === 'Sinh Hoạt.html') {
     //  return f.render();
--- vite.config.js
+++ vite.config.js
@@ -15,7 +15,7 @@ export default defineConfig({
         'Đoàn Thể': resolve(__dirname, 'Đoàn Thể.html'),
         //'Giáo Xứ': resolve(__dirname, 'Giáo Xứ.html'),
         //'Mục Vụ': resolve(__dirname, 'Mục Vụ.html'),
-        'phụng vụ': resolve(__dirname, 'phụng vụ.html'),
+        'phung vu': resolve(__dirname, 'phung vu.html'),
         //'Sinh Hoạt': resolve(__dirname, 'Sinh Hoạt.html'),
         //'Thông Tin': resolve(__dirname, 'Thông Tin.html'),
       },
